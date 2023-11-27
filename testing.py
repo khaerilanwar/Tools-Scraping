9bbe241977258285bb97313a74f4f58033b9bafc
khaerilanwar <khaerilanwar1992@gmail.com>
2023-11-27 14:01:35 +0700

    done scrap wedew

--- testing.py
+++ testing.py
@@ -3,32 +3,72 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
 
 # URL Target
 url = "https://wedew.id/tema"
 chrome_options = Options()
+chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 # chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(chrome_options)
 driver.implicitly_wait(10)
 driver.get(url)
-next_page = driver.find_element(
-    By.CSS_SELECTOR,
-    "body > div.container > div:nth-child(5) > nav > ul > li:last-child",
-)
-# while True:
-#     button_items = driver.find_elements(
-#         By.CSS_SELECTOR, "div.row.theme-list > div[data-request-data] > a.theme-item"
+
+# try:
+#     next_page = driver.find_element(
+#         By.CSS_SELECTOR,
+#         "nav > ul.pagination > li.page-item > a[aria-label='pagination.next']",
 #     )
-#     links_item = [link.get_attribute("href") for link in button_items]
-#     for link_item in links_item:
-#         driver.get(link_item)
-#         title_item = driver.find_element(
-#             By.CSS_SELECTOR, "body > div.page-header > div > h1"
-#         ).text
-#         print(title_item)
-#     if "disabled" not in next_page.get_attribute("class"):
-#         next_page.click()
-#     else:
+#     driver.get(next_page.get_attribute('href'))
+# except:
+#     print('Tidak ada')
+
+# print(next_page.click())
+
+# while True:
+#     try:
+#         next_page = driver.find_element(
+#             By.CSS_SELECTOR,
+#             "nav > ul.pagination > li.page-item > a[aria-label='pagination.next']",
+#         )
+#         link_next_page = next_page.get_attribute('href')
+#         print(link_next_page)
+#         driver.get(link_next_page)
+#     except NoSuchElementException:
+#         print('selesai...')
 #         break
+
+while True:
+    try:
+        current_url = driver.current_url
+        next_page = driver.find_element(
+            By.CSS_SELECTOR,
+            "nav > ul.pagination > li.page-item > a[aria-label='pagination.next']",
+        )
+        link_next_page = next_page.get_attribute('href')
+        button_items = driver.find_elements(
+            By.CSS_SELECTOR, "div.row.theme-list > div[data-request-data] > a.theme-item"
+        )
+        links_item = [link.get_attribute("href") for link in button_items]
+        for link_item in links_item:
+            driver.get(link_item)
+            title_item = driver.find_element(
+                By.CSS_SELECTOR, "body > div.page-header > div > h1"
+            ).text
+            print(title_item)
+        print(link_next_page)
+        driver.get(link_next_page)
+    except NoSuchElementException:
+        print('Selesai ...')
+        break
+
+# next_page.click()
+# if "disabled" not in next_page.get_attribute("class"):
+#     next_page.click()
+#     print('bisa di klik')
+# else:
+#     print('tidak bisa di klik')
+#     break
+
 time.sleep(5)
 driver.quit()
--- wedew.py
+++ wedew.py
@@ -4,6 +4,7 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
 
 
 # URL Target
@@ -14,89 +15,91 @@ url = "https://wedew.id/tema"
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--log-level=3")
+chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(options=chrome_options)
 driver.implicitly_wait(10)
 
 # Melakukan request get kedalam url target
 driver.get(url)
 
-next_page = driver.find_element(
-    By.CSS_SELECTOR,
-    "body > div.container > div:nth-child(5) > nav > ul > li:last-child",
-)
-
 while True:
-    button_items = driver.find_elements(
-        By.CSS_SELECTOR, "div.row.theme-list > div[data-request-data] > a.theme-item"
-    )
-    links_item = [link.get_attribute("href") for link in button_items]
-
-    # Melakukakn looping kedalam tiap item theme
-    for link_item in links_item:
-        driver.get(link_item)
-        # Mendapatkan judul template wedding
-        title_item = driver.find_element(
-            By.CSS_SELECTOR, "body > div.page-header > div > h1"
-        ).text
-
-        # Membuat folder untuk menyimpan data template
-        save_dir = os.path.join("wedew", title_item)
-        os.mkdir(save_dir)
-
-        # Mendownload gambar preview template
-        img_desktop = driver.find_element(
-            By.CSS_SELECTOR, "div.theme-detail-preview-desktop > img"
+    try:
+        next_page = driver.find_element(
+            By.CSS_SELECTOR,
+            "nav > ul.pagination > li.page-item > a[aria-label='pagination.next']",
         )
-        img_mobile = driver.find_element(
-            By.CSS_SELECTOR, "div.theme-detail-preview-mobile > img"
+        link_next_page = next_page.get_attribute('href')
+        button_items = driver.find_elements(
+            By.CSS_SELECTOR, "div.row.theme-list > div[data-request-data] > a.theme-item"
         )
-        url_img_desktop = img_desktop.get_attribute("src")
-        with open(
-            os.path.join(
-                save_dir, f"{title_item} Desktop.{url_img_desktop.split('.')[-1]}"
-            ),
-            "wb",
-        ) as file:
-            file.write(requests.get(url_img_desktop).content)
+        links_item = [link.get_attribute("href") for link in button_items]
 
-        url_img_mobile = img_mobile.get_attribute("src")
-        with open(
-            os.path.join(
-                save_dir, f"{title_item} Mobile.{url_img_mobile.split('.')[-1]}"
-            ),
-            "wb",
-        ) as file:
-            file.write(requests.get(url_img_mobile).content)
+        # Melakukakn looping kedalam tiap item theme
+        for link_item in links_item:
+            driver.get(link_item)
+            # Mendapatkan judul template wedding
+            title_item = driver.find_element(
+                By.CSS_SELECTOR, "body > div.page-header > div > h1"
+            ).text
 
-        # Menyimpan tab utama
-        main_tab = driver.current_window_handle
+            # Membuat folder untuk menyimpan data template
+            save_dir = os.path.join("wedew", title_item)
+            os.mkdir(save_dir)
 
-        # Melakukan redirect ke halaman preview
-        preview_button = driver.find_element(By.LINK_TEXT, "Preview")
-        driver.get(preview_button.get_attribute("href"))
-        see_full = driver.find_element(By.LINK_TEXT, "Lihat Penuh")
-        see_full.click()
+            # Mendownload gambar preview template
+            img_desktop = driver.find_element(
+                By.CSS_SELECTOR, "div.theme-detail-preview-desktop > img"
+            )
+            img_mobile = driver.find_element(
+                By.CSS_SELECTOR, "div.theme-detail-preview-mobile > img"
+            )
+            url_img_desktop = img_desktop.get_attribute("src")
+            with open(
+                os.path.join(
+                    save_dir, f"{title_item} Desktop.{url_img_desktop.split('.')[-1]}"
+                ),
+                "wb",
+            ) as file:
+                file.write(requests.get(url_img_desktop).content)
 
-        # Dapatkan semua tab
-        all_tabs = driver.window_handles
+            url_img_mobile = img_mobile.get_attribute("src")
+            with open(
+                os.path.join(
+                    save_dir, f"{title_item} Mobile.{url_img_mobile.split('.')[-1]}"
+                ),
+                "wb",
+            ) as file:
+                file.write(requests.get(url_img_mobile).content)
 
-        for tab in all_tabs:
-            if tab != main_tab:
-                driver.switch_to.window(tab)
-                # Menyimpan page source
-                with open(
-                    os.path.join(save_dir, f"{title_item}.html"), "w", encoding="utf-8"
-                ) as file:
-                    file.write(driver.page_source)
+            # Menyimpan tab utama
+            main_tab = driver.current_window_handle
 
-                driver.close()
-                break
+            # Melakukan redirect ke halaman preview
+            preview_button = driver.find_element(By.LINK_TEXT, "Preview")
+            driver.get(preview_button.get_attribute("href"))
+            see_full = driver.find_element(By.LINK_TEXT, "Lihat Penuh")
+            see_full.click()
 
-        driver.switch_to.window(main_tab)
+            # Dapatkan semua tab
+            all_tabs = driver.window_handles
 
-    if "disabled" not in next_page.get_attribute("class"):
-        next_page.click()
-    else:
+            for tab in all_tabs:
+                if tab != main_tab:
+                    driver.switch_to.window(tab)
+                    # Menyimpan page source
+                    with open(
+                        os.path.join(save_dir, f"{title_item}.html"), "w", encoding="utf-8"
+                    ) as file:
+                        file.write(driver.page_source)
+
+                    driver.close()
+                    break
+            print(f"Menambahkan {title_item} ..")
+
+            driver.switch_to.window(main_tab)
+        driver.get(link_next_page)
+    except NoSuchElementException:
+        print('Selesai ..')
         break
 
 time.sleep(5)
